71eddc86f558f111abc8e9a93fbf959f4a004132
KennAdams <kenn.adams200@gmail.com>
2023-09-24 04:00:01 +0300

    add basic scraping functions again

--- Flask API/api/scrapeNewsPages.py
+++ Flask API/api/scrapeNewsPages.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from lxml import etree
+from rich import print
 
 newsJSON = {}
 
@@ -15,8 +16,11 @@ def scrapeReuters(url):
     responseSoup=BeautifulSoup(response.content, 'html.parser')
     responseDom=etree.HTML(str(responseSoup))
     newscards=responseSoup.find_all('div', {"data-testid":"MediaStoryCard"})
-    #print(responseDom.xpath("//*[@class='media-story-card__playlist__2ESDd story-card']"))
-
+    link=newscards[5].a['href']
+    image_src=newscards[5].a.img['src']
+    title=newscards[5].h3.text
+    time=newscards[5].time.text
+    logo_src='https://1000logos.net/wp-content/uploads/2017/06/Reuters-Logo-500x333.png'
 
 def scrapeCNN(url):
     url = 'https://edition.cnn.com/'+url+'/'
